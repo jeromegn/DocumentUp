796cd879e10a010f435cfcc5af745ef3a6b79321
Jerome Gravel-Niquet <jeromegn@gmail.com>
2012-07-15 18:16:27 -0400

    again?

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,5 +1,5 @@
 make install
-stylus app/stylesheets/screen.styl -o public/stylesheets --use nib
-stylus app/stylesheets/themes -o public/stylesheets/themes --use nib
+./node_modules/.bin/stylus app/stylesheets/screen.styl -o public/stylesheets --use nib
+./node_modules/.bin/stylus app/stylesheets/themes -o public/stylesheets/themes --use nib
 /etc/init.d/documentup stop
 /etc/init.d/documentup start
