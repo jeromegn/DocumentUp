a65b8e0b9863808eaeab9ef426f1486f8c701fb9
Jerome Gravel-Niquet <jeromegn@gmail.com>
2013-07-09 12:25:44 -0400

    adjust post_deploy for reliability

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,4 +1,5 @@
 make install
+mkdir -p public/stylesheets/themes
 ./node_modules/.bin/stylus app/stylesheets/screen.styl -o public/stylesheets --use nib
 ./node_modules/.bin/stylus app/stylesheets/themes -o public/stylesheets/themes --use nib
 /etc/init.d/documentup stop
