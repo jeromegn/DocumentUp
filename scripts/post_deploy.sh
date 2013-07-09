d5351a8fe8c704826295da7bf8cfa4e63cdc8066
Jerome Gravel-Niquet <jeromegn@gmail.com>
2013-07-09 12:15:24 -0400

    no need to source..

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,4 +1,3 @@
-source ~/.profile
 make install
 ./node_modules/.bin/stylus app/stylesheets/screen.styl -o public/stylesheets --use nib
 ./node_modules/.bin/stylus app/stylesheets/themes -o public/stylesheets/themes --use nib
