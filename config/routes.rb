90d1f7106bbc427ee3de473db7707ee1d4a56f6f
Jerome Gravel-Niquet <jeromegn@gmail.com>
2015-06-15 15:10:22 -0400

    redirect old screen link to new stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/', to: redirect('/jeromegn/documentup')
   post :recompile, to: 'github#push', as: :github_push
 
+  get '/stylesheets/screen.css', to: redirect('/assets/application.css')
+
   resources :users, path: '', param: :login, only: [] do
     resources :repositories, path: '', param: :name, constraints: {name: /([A-Za-z0-9\._]*)/}, only: [:show] do
       get :__recompile, to: 'repositories#recompile', as: :recompile
